6e455a3d88c65e1ea216520e07bc0691aeb59179
Olalekan Ilori <47522482+Parakconcepts@users.noreply.github.com>
2022-08-23 07:38:40 -0400

    Add files via upload
    Linux fundamentals project- Bash script displaying Operating system information and refreshing every 10 seconds

---
+++ LNF-Project.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while [ 1==1 ]
+do 
+
+echo "Linux Version: $(uname -a |awk '{print $2,$3}')";
+echo "Private IP: $(ifconfig|grep inet|head -n1|awk '{print $2}')"
+echo -e "Public IP: $(curl -s  ifconfig.co)" 
+echo "Default Gateway: $(route -n|grep UG |awk '{print $2}')"
+echo "Disk Size: $(fdisk -l|grep Disk|head -n1|awk '{print $3$4}'|tr "," " ")"
+echo "Disk Size: $(df -H|grep sda|awk '{print $2}') "
+echo "Used  Disk Space: $(df -H|grep sda|awk '{print $3}') "
+echo "Available Disk Space: $(df -H|grep sda|awk '{print $4}') "
+echo "Top 5 Directories By Size: "
+echo "---------------------------"
+echo "$(ls / -lSah |grep root|head -5|awk '{print $5,$9}')"
+echo "CPU Usage Statistics:"
+echo "---------------------------"
+echo -e "$(mpstat)"
+sleep 10
+clear
+done
